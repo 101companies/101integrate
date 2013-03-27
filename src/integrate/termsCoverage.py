186c1b7ca74f3807e41ad1100625a894a1b3c498
Thomas Schmorleiz <tschmorleiz@googlemail.com>
2013-03-27 13:31:49 +0100

    Typo

--- src/integrate/termsCoverage.py
+++ src/integrate/termsCoverage.py
@@ -79,7 +79,7 @@ for theme in themes:
   	createTable(dict([(c['name'],c['links']) for c in contribs]), 'themes', theme)
 
 for language in languages:
- 	print 'Table for', theme
+ 	print 'Table for', language
 	contribs = query(allLanguageUsers). \
   	where(lambda page: filter(lambda p: p['p'] == 'Language' and p['n'] == language and 'internal_links' in page['page'], page['page']['uses'])). \
   	select(lambda page: {'name': page['page']['page']['n'], 'links': map(lambda l: l.lower().split('::')[-1],page['page']['internal_links'])}). \
