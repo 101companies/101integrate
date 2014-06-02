0fdbe14fb6cde8c2300ffb04b9a7f2821c9d96b6
Michael Monschau <github@michael-monschau.de>
2014-06-02 20:17:28 +0200

    preparing for selective Book-Download

---
+++ src/mining/bookDownloader.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import simplejson as json
+import sys
+
+def normalizeString(string):
+  return string.strip().strip(" ").lower()
+
+def nameEquals(string1, string2):
+  if(normalizeString(string1) == normalizeString(string2)):
+    return True
+  else:
+    return False
+
+books = set();
+
+bookData = json.loads(open("../config.json", 'rb').read()) #TODO read as hirachical object
+print "read config:"
+print bookData
+
+if nameEquals("all", sys.argv[1]):
+  for data in bookData:
+    books.add(data)
+else:
+  for arg in sys.argv:
+    print " comparing " + arg
+    for data in bookData:
+      print "\t with " + data
+      if (arg  == data ): # or arg == data.fullname or arg in data.package): 
+	print "\t\t"+ arg+" recognized"
+	books.add(data)
+	print "\t\t"+ data+" added "
+	
+print "Books to be downloaded:"
+print books
+#TODO calling CRAWLER with books as args
