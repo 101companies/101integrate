ad9e77fa5b236d79ee347794fc9122915ee73b82
Michael Monschau <github@michael-monschau.de>
2014-06-11 20:16:51 +0200

    changed from string operations to regex

--- src/mining/metadataGenerator.py
+++ src/mining/metadataGenerator.py
@@ -2,15 +2,51 @@ import sys
 import urllib2
 import simplejson as json
 import os
+import re
 from bs4 import BeautifulSoup
 
 dataPath = ".."+os.path.sep+".."+os.path.sep+"data"
 
 def getMetaPath(book):
-    return dataPath+os.path.sep+"perbook"+os.path.sep+book+os.path.sep+"metadata"+os.path.sep
-  
+    path = dataPath+os.path.sep+"perbook"+os.path.sep+book+os.path.sep+"metadata"+os.path.sep
+    try:
+      os.makedirs(path)
+      print path + "was created"
+    except OSError:
+      pass
+    else:
+      pass
+    return path
+
+
+
+
+
 def isRelative(url):
-     return (not (("http://" in url) or ("https://" in url)))
+     #return (not (("http://" in url) or ("https://" in url)))
+     return (re.match("http(s)?://", url) is not None)
+
+
+
+#DEPRECATED replaced with acceptor
+def isAccepted(url, ext):
+    return (not ("mailto:" in url) and ((not ("http:" in url)) or url in url) and (ext in url))
+
+
+
+
+
+def createAcceptor(urlBase, ext):
+    regex=""
+    for url in urlBase.replace(".","\.").split("/"):
+	regex += ("("+url+"/)?")
+    regex += "/?"+"((-|_|[A-Za-z0-9])*/?){1,2}"+ext.replace(".","\.")
+    print regex
+    return re.compile(regex)
+
+
+
+
 
 if (len(sys.argv)<=1):
     print "No book specified. Aborting."
@@ -22,16 +58,17 @@ else:
 	try:
 	    links = []
 	    url = bookData[arg]['urlBase'].strip()
+	    print "Regex-Term:"
+	    acceptor = createAcceptor(url, bookData[arg]['ext'])
 	    print "checking " + url
 	    infile = opener.open(url)
 	    if (infile.info()['content-type'].count('text/html') > 0):
 		  doc = BeautifulSoup(infile.read())
 		  for l in doc.find_all('a'):
 		      info = [l, l.get('href'), l.string]
-		      if((not ("mailto:" in info[1])) and 
-			((not ("http:" in info[1])) or url in info [1]) and 
-			(bookData[arg]['ext'] in info[1])):
-			  links.append(info)
+		      if info[1] is not None:
+			  if acceptor.match(info[1]) is not None:
+			      links.append(info)
 	    print "collected Information"
 	    path = getMetaPath(arg)
 	    chaptersWrite = open(path+"chaptersGen.txt","w")
@@ -39,8 +76,8 @@ else:
 	    print "Processing links"
 	    for l in links:
 		    print l
-		    if(isRelative(l[1])):
-		      chaptersWrite.write(bookData[arg]['urlBase'].strip()+l[1])
+		    if isRelative(l[1]):
+		      chaptersWrite.write(url+l[1])
 		      filename = l[1]+".txt"
 		      chapters.append({"file": filename ,"title": l[2]})
 		    else:
