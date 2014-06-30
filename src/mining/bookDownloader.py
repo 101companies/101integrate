836d8d77dbc3ea0fd4b12fa0c55e6f1c3f615642
Michael Monschau <github@michael-monschau.de>
2014-06-30 16:18:25 +0200

    updated diagrams

--- src/mining/bookDownloader.py
+++ src/mining/bookDownloader.py
@@ -5,6 +5,17 @@ import os
 import crawler
 
 ##
+#@param	path	a directory you want to create
+# creates a directory if it does not exist
+def mkdir(path):
+    try:
+      os.makedirs(path)
+    except OSError:
+      pass
+    else:
+      pass
+
+##
 #@param compared	the string to be compared
 #@param strings		an array of strings which are compared with the compared
 #@return 	wether the compared matches another sting in "normal form"
@@ -28,9 +39,13 @@ def normalizeString(string):
 
 
 
-def selectBooks(args):
+##
+#@param		args	the books you want to download
+#@param 	config	the configuration
+#@return	a set containg books matching with args
+def selectBooks(args, config):
   books = set();
-  bookData = json.loads(open("config"+os.path.sep+"config.json", 'rb').read())
+  bookData = config
 
   if (nameIsIn("all",[args[1]]) or len(args) == 1 ):
     for data in bookData:
@@ -60,22 +75,25 @@ def selectBooks(args):
 
 
 
-
-def downloadBooks(books):
+##
+#@param	books		the books you want to download
+#@param bookfldr	the folder in which the book should be downloaded
+# downloads te specified books in the specified folder
+def downloadBooks(books, bookfldr):
   print "Books to be downloaded:"
   print books
+  if bookfldr[len(bookfldr)-1] is not ("/" or os.path.sep):
+      bookfldr += os.path.sep
+  bookfldr = bookfldr.replace("/",os.path.sep)
   for b in books:
-    try:
-      os.makedirs(("../../data/perbook/"+b+"/contents").replace("/",os.path.sep))
-    except OSError:
-      pass
-    else:
-      pass
-    crawler.crawl([sys.argv[0],b,("../../data/perbook/").replace("/",os.path.sep)])
-    #sys.argv = [sys.argv[0],b,("../../data/perbook/").replace("/",os.path.sep)]
-    #import crawler
-    #print subprocess.Popen("python crawler.py "+b+" ../../data/perbook/".replace("/",os.path.sep), shell = True).wait()
+      mkdir((bookfldr+b+os.path.sep+"contents"))
+      crawler.crawl([sys.argv[0],b,bookfldr])
+      #sys.argv = [sys.argv[0],b,("../../data/perbook/").replace("/",os.path.sep)]
+      #import crawler
+      #print subprocess.Popen("python crawler.py "+b+" ../../data/perbook/".replace("/",os.path.sep), shell = True).wait()
+  
+  
     
     
 if __name__ == "__main__":
-   downloadBooks(selectBooks(sys.argv))
+   downloadBooks(selectBooks(sys.argv, json.loads(open("config"+os.path.sep+"config.json", 'rb').read())),"../../data/perbook/")
